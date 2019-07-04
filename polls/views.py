a4411523f7a16c2ee708a0f1aeb0e3dfae39825e
Rodrigo Schwartz <rodrigoalexschwartz@gmail.com>
2019-07-04 11:14:04 -0300

    2

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
+from django.shortcuts import render
 from django.http import HttpResponse
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def post_list(request):
+    return render(request, 'polls/post_list.html', {})
